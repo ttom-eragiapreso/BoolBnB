c055d2af3083f153693a648bf7d6a61f88f94c24
ttom-eragiapreso <109075301+ttom-eragiapreso@users.noreply.github.com>
2023-02-10 16:47:12 +0100

    INIT

---
+++ resources/js/Pages/Houses.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1>Ciao Houses</h1>
+    <h2>{{ message }}</h2>
+    <Link :href="route('prova')" :data="{ message: 'ciao' }"> Prova </Link>
+</template>
+
+<!-- <script>
+import { Link } from "@inertiajs/vue3";
+export default {
+    props: {
+        message: String,
+    },
+    components: {
+        Link,
+    },
+};
+</script> -->
+
+<script setup>
+import { Link } from "@inertiajs/vue3";
+
+let props = defineProps({
+    message: String,
+});
+</script>
