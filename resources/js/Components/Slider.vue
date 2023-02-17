c6fc82320be501bbd2e409d132780e0e7b758a97
mlietti1 <mcf.lietti@outlook.com>
2023-02-17 16:00:43 +0100

    added components slider types

---
+++ resources/js/Components/Slider.vue
@@ -0,0 +1,107 @@
+<script>
+
+import SliderItem from './SliderItem.vue';
+
+export default {
+    name: 'Slider',
+    components: {
+        SliderItem
+    },
+    props:{
+        types_of_stay: Array
+    },
+    data(){
+      return {
+        scroll_position: 0,
+      }
+
+    },
+    methods: {
+    scroll_left() {
+      let content = document.querySelector(".wrapper-box");
+      content.scrollLeft -= 400;
+    },
+    scroll_right() {
+      let content = document.querySelector(".wrapper-box");
+      content.scrollLeft += 400;
+    },
+    scrollHandle() {
+      let content = document.querySelector(".wrapper-box");
+      console.log(content.scrollLeft);
+      this.scroll_position = content.scrollLeft;
+      return this.scroll_position
+    }
+  },
+}
+
+</script>
+
+<template>
+  <div>
+
+    <div class="flex relative">
+      <div class="btn-wrap-l sm:flex hidden absolute z-10 left-0" v-if="this.scroll_position > 15">
+        <button @click="scroll_left"><i class="fa-solid fa-chevron-left"></i></button>
+      </div>
+      <div @scroll="scrollHandle()" class="px-6 flex wrapper-box hide-scroll w-screen overflow-auto">
+
+        <SliderItem  v-for="item in types_of_stay" :item="item" :key="item.id"/>
+        <SliderItem  v-for="item in types_of_stay" :item="item" :key="item.id"/>
+      </div>
+      <div class="sm:flex hidden btn-wrap-r absolute z-10 right-0">
+
+        <button class="sm:block hidden" @click="scroll_right"><i class="fa-solid fa-chevron-right z-20"></i></button>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+.hide-scroll::-webkit-scrollbar {
+    display: none;
+  }
+
+.hide-scroll{
+    -ms-overflow-style: none;  /* IE and Edge */
+    scrollbar-width: none;  /* Firefox */
+    scroll-behavior: smooth;
+
+}
+
+button {
+  border: 1px solid lightgray;
+  border-radius: 50%;
+  height: 28px;
+  width: 28px;
+  line-height: 28px;
+
+  transition: all 300ms;
+  &:hover{
+    box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+    scale: 1.05;
+
+  }
+}
+
+.btn-wrap-l{
+
+  align-items: center;
+  height: 79px;
+  padding-right: 50px;
+  background: rgb(255,255,255);
+background: linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 55%, rgba(255,255,255,0.9) 70%, rgba(255,255,255,0.7) 85%, rgba(255,255,255,0) 100%);
+}
+
+.btn-wrap-r{
+
+  align-items: center;
+
+  height: 79px;
+  padding-left:50px;
+  background: rgb(255,255,255);
+background: linear-gradient(270deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 55%, rgba(255,255,255,0.9) 70%, rgba(255,255,255,0.7) 85%, rgba(255,255,255,0) 100%);
+}
+
+</style>
---
+++ resources/js/Components/SliderItem.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+    name: 'SliderItem',
+    props:{
+        item: Object
+    }
+}
+</script>
+
+<template>
+
+    <div class=" cursor-pointer flex flex-col text-lg items-center mx-8 my-4 text-slate-500 hover:text-black transition duration-300 hover:ease-in">
+        <i :class=" item.icon "></i>
+        <span>{{ item.name }}</span>
+    </div>
+
+</template>
+
+<style scoped lang="scss">
+
+@import '../../../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
+
+div{
+    border-bottom: 2px solid transparent;
+    &:hover{
+        border-bottom-color: lightgray;
+    }
+}
+
+i{
+    font-size: 1.2rem;
+}
+span{
+    font-size: 0.8rem;
+    white-space: nowrap;
+}
+</style>
