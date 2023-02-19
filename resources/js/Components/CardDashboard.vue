9706645a7c1c3f718a041206de7bd79bd587fb40
ttom-eragiapreso <109075301+ttom-eragiapreso@users.noreply.github.com>
2023-02-19 10:25:57 +0100

    Aggiunto confirm al delete

--- resources/js/Components/CardDashboard.vue
+++ resources/js/Components/CardDashboard.vue
@@ -15,18 +15,26 @@
                 <h2 class="font-bold text-3xl">{{ apartment.title }}</h2>
             </div>
             <!-- Location -->
-            <h5 class="text-slate-600">{{ apartment.full_address }}, {{ apartment.city }},  {{ apartment.country }}</h5>
+            <h5 class="text-slate-600">
+                {{ apartment.full_address }}, {{ apartment.city }},
+                {{ apartment.country }}
+            </h5>
             <!-- Price -->
             <h5 class="">
                 <strong>{{ apartment.price }} &euro;</strong> / night
             </h5>
             <!-- Public -->
             <h5 class="text-slate-600 mb-auto">
-                Is Public: <strong>{{ apartment.is_visible ? "Yes" : "No" }}</strong>
+                Is Public:
+                <strong>{{ apartment.is_visible ? "Yes" : "No" }}</strong>
             </h5>
             <!-- Date -->
-            <h5 class="text-slate-600">Created at: {{ apartment.created_at }}</h5>
-            <h5 class="text-slate-600">Last update at: {{ apartment.updated_at }}</h5>
+            <h5 class="text-slate-600">
+                Created at: {{ apartment.created_at }}
+            </h5>
+            <h5 class="text-slate-600">
+                Last update at: {{ apartment.updated_at }}
+            </h5>
         </div>
 
         <div class="px-7 flex flex-col gap-5 justify-center">
@@ -44,26 +52,18 @@
             >
                 Edit
             </Link>
-            <Link
-                :href="route('dashboard.apartment.destroy', apartment)"
-                @click.prevent="remove"
-                method="delete"
-                as="button"
+            <button
+                @click.prevent="remove(apartment)"
                 class="px-6 py-3 bg-red-600 border border-transparent rounded-md font-bold text-xs text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150"
-
             >
                 Delete
-            </Link>
-            <!-- <button @click="remove" class="border rounded-md px-3" >Delete</button> -->
+            </button>
         </div>
     </div>
 </template>
 
 <script>
-
-
-import { Link } from "@inertiajs/vue3";
-//import {Inertia} from "@inertiajs/inertia-vue3";
+import { Link, router } from "@inertiajs/vue3";
 
 export default {
     name: "CardDashboard",
@@ -73,10 +73,12 @@ export default {
     components: {
         Link,
     },
-    methods:{
-        remove(){
-            return confirm('Are you sure?')
-}
-    }
+    methods: {
+        remove(apartment) {
+            if (confirm("Are you sure?")) {
+                router.delete(route("dashboard.apartment.destroy", apartment));
+            }
+        },
+    },
 };
 </script>
