446cc2fc5b4065eebf84dc88861db78ba7f66185
ttom-eragiapreso <109075301+ttom-eragiapreso@users.noreply.github.com>
2023-02-20 18:10:57 +0100

    aggiunto cards alla home

--- resources/js/Components/Card.vue
+++ resources/js/Components/Card.vue
@@ -3,13 +3,35 @@
         <div>
             <!-- Img container -->
             <div
-                class="w-72 aspect-square border rounded-xl overflow-hidden relative"
+                class="w-60 aspect-square border rounded-xl overflow-hidden relative"
             >
-                <img
-                    src="https://static.designboom.com/wp-content/uploads/2019/12/niko-architect-house-in-the-landscape-moscow-designboom-1800.jpg"
-                    alt="Immagine"
-                    class="h-full w-full object-cover"
-                />
+                <swiper
+                    class="w-full h-full"
+                    :loop="true"
+                    navigation
+                    :mousewheel="{
+                        forceToAxis: true,
+                    }"
+                    :pagination="{
+                        clickable: true,
+                        dynamicBullets: true,
+                    }"
+                >
+                    <swiper-slide>
+                        <img
+                            :src="'/storage/' + apartment.cover_image"
+                            alt="Immagine"
+                            class="h-full w-full object-cover"
+                        />
+                    </swiper-slide>
+                    <swiper-slide v-for="image in apartment.images">
+                        <img
+                            :src="'/storage/' + image.url"
+                            alt="Immagine"
+                            class="h-full w-full object-cover"
+                        />
+                    </swiper-slide>
+                </swiper>
 
                 <div class="absolute top-0 right-0 z-10 text-white p-3 text-xl">
                     <i class="fa-regular fa-heart"></i>
@@ -33,4 +55,26 @@
     </div>
 </template>
 
-<script setup></script>
+<script>
+import { Swiper, SwiperSlide } from "swiper/vue";
+import { Navigation, Pagination } from "swiper";
+
+import "swiper/scss";
+import "swiper/scss/navigation";
+import "swiper/scss/pagination";
+import "swiper/scss/scrollbar";
+import "swiper/scss/mousewheel";
+
+export default {
+    components: {
+        Swiper,
+        SwiperSlide,
+        Navigation,
+        Pagination,
+    },
+    props: {
+        apartment: Object,
+    },
+};
+</script>
+<style lang="scss" scoped></style>
--- resources/js/Pages/Guest/Home.vue
+++ resources/js/Pages/Guest/Home.vue
@@ -1,12 +1,14 @@
 <script>
 import GuestLayout from "@/Layouts/GuestLayout.vue";
 import Slider from "@/Components/Slider.vue";
+import Card from "@/Components/Card.vue";
 
 export default {
     name: "Home",
     components: {
         GuestLayout,
         Slider,
+        Card,
     },
     props: {
         types_of_stay: Array,
@@ -19,6 +21,14 @@ export default {
     <GuestLayout>
         <div class="px-8 lg:px-20 bg-white">
             <Slider :types_of_stay="types_of_stay" />
+        </div>
+        <div class="flex gap-4 flex-wrap">
+            <Card
+                v-for="apartment in apartments"
+                :key="apartment.id"
+                :apartment="apartment"
+                class="w-auto"
+            />
         </div>
     </GuestLayout>
 </template>
