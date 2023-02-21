9416c0ce29570be4f616bbd48405ab11721ac852
pannellalino <lista>
2023-02-21 16:38:49 +0100

    correction

--- resources/js/Pages/Guest/Details.vue
+++ resources/js/Pages/Guest/Details.vue
@@ -24,6 +24,7 @@ export default {
     methods:{
         toggleModal: function(){
         this.showModal = !this.showModal;
+        }
     },
     mounted() {
         const map = tt.map({
@@ -48,8 +49,7 @@ export default {
             map.addControl(new tt.NavigationControl);
         });
 
-    }
-    }
+     }
 };
 </script>
 
