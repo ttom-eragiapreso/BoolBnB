3a8df5baf14ba9fc7474dde1018fc3322319c1a6
ttom-eragiapreso <109075301+ttom-eragiapreso@users.noreply.github.com>
2023-02-22 15:38:18 +0100

    creato component delete button con modal

--- resources/js/Components/DeleteButton.vue
+++ resources/js/Components/DeleteButton.vue
@@ -1,6 +1,6 @@
 <template>
     <button
-        @click.prevent="remove(apartment)"
+        @click.prevent="$emit('deleteApartment')"
         class="px-1 md:px-8 py-3 bg-red-600 border border-transparent rounded-md font-bold text-xs text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150"
     >
         <i class="fa-solid fa-trash text-sm"></i>
