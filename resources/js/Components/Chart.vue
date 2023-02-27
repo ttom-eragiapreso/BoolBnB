275ee98c59c68842d458a0f9282a6669b167d553
mlietti1 <mcf.lietti@outlook.com>
2023-02-27 18:09:58 +0100

    created chart

---
+++ resources/js/Components/Chart.vue
@@ -0,0 +1,73 @@
+<template>
+    <Line :data="data" :options="options" />
+  </template>
+
+  <script>
+  import {
+    Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+  } from 'chart.js'
+  import { Line } from 'vue-chartjs'
+
+  ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+)
+
+  export default {
+    name: 'Chart',
+    components: {
+      Line
+    },
+    props:{
+        my_data: Object
+    },
+    data() {
+      return {
+        colors: ['red', 'yellow', 'blue', 'green', 'burlywood', 'cornflowerblue', 'coral', 'darkblue'],
+        data : {
+            datasets: []
+        },
+
+
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            parsing: {
+                xAxisKey: 'date',
+                yAxisKey: 'total'
+            }
+        },
+
+      }
+    },
+    created(){
+
+        console.log(this.data.datasets);
+
+        for(let apartment in this.my_data){
+            this.data.datasets.push({
+                data: this.my_data[apartment],
+                label: apartment,
+                borderColor: this.colors[Math.floor(Math.random()* this.colors.length)],
+                backgroundColor: this.colors[Math.floor(Math.random()* this.colors.length)],
+
+            })
+        }
+
+
+    }
+  }
+  </script>
+
